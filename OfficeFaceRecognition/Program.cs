43a3923d19f4673c48f3fbc66084ebf93ccbd07c
BogdanGrytsko <bgrytsko@gmail.com>
2019-09-24 02:23:58 +0300

    added resizing, blob from image, detector forward, maxConfidence.

--- OfficeFaceRecognition/Program.cs
+++ OfficeFaceRecognition/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using CommandLine;
+using Emgu.CV;
 using Emgu.CV.Dnn;
+using Emgu.CV.Structure;
 
 namespace OfficeFaceRecognition
 {
@@ -25,6 +28,34 @@ namespace OfficeFaceRecognition
             var embedder = DnnInvoke.ReadNet(facePars.EmbeddingModel);
             Console.WriteLine("[INFO] quantifying faces...");
             var imagePaths = Directory.GetFiles(facePars.DataSet, "*.*", SearchOption.AllDirectories);
+            foreach (var imagePath in imagePaths)
+            {
+                var personName = Path.GetFileName(Path.GetDirectoryName(imagePath));
+                Console.WriteLine($"Processing image {imagePath}");
+                var image = CvInvoke.Imread(imagePath);
+                //todo : here was the step with resizing to 600, investigate
+                //image = imutils.resize(image, width = 600)
+                //    (h, w) = image.shape[:2]
+                var resizedThreeHundred = new Mat();
+                CvInvoke.Resize(image, resizedThreeHundred, new Size(300, 300));
+                var blobFromImage = DnnInvoke.BlobFromImage(
+                    resizedThreeHundred, 1, new Size(300, 300), new MCvScalar(104.0, 177.0, 123.0));
+                detector.SetInput(blobFromImage);
+                var detection = detector.Forward();
+                var data = (float[,,,]) detection.GetData();
+                var maxi = 0;
+                for (int i = 0; i < detection.SizeOfDimension[2]; i++)
+                {
+                    if (data[0, 0, i, 2] > maxi)
+                        maxi = i;
+                }
+
+                var confidence = data[0, 0, maxi, 2];
+                if (confidence > facePars.Confidence)
+                {
+                    Console.WriteLine($"Confident {confidence}, computing boxes");
+                }
+            }
         }
 
         private static void HandleParseError(IEnumerable<Error> errors)
