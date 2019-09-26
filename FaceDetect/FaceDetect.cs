150191bb2b32f7b6b4c6a3bd085c505dee770f18
BogdanGrytsko <bgrytsko@gmail.com>
2019-09-27 00:36:47 +0300

    Made FaceDetect project buildable, minor refactoring, updated it to 4.7.2, used emgu from nuget.

--- FaceDetect/FaceDetect.cs
+++ FaceDetect/FaceDetect.cs
@@ -16,8 +16,8 @@ using Emgu.CV.UI;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using System.Runtime.InteropServices;
-using FaceDetection;
 using System.Threading;
+using FaceDetect.Detection;
 
 namespace VideoSurveillance
 {
@@ -26,11 +26,11 @@ namespace VideoSurveillance
         [DllImport("msvcrt.dll")]
         public static extern int _putenv_s(string e, string v);
 
-        private static VideoCapture _cameraCapture;
+        private static VideoCapture cameraCapture;
 
-        private static IBackgroundSubtractor _fgDetector;
-        private static Emgu.CV.Cvb.CvBlobDetector _blobDetector;
-        private static Emgu.CV.Cvb.CvTracks _tracker;
+        private static IBackgroundSubtractor fgDetector;
+        private static CvBlobDetector blobDetector;
+        private static CvTracks tracker;
 
         public FaceDetect()
         {
@@ -46,7 +46,7 @@ namespace VideoSurveillance
 
                 _putenv_s("OPENCV_FFMPEG_CAPTURE_OPTIONS", "rtsp_transport;udp");
                 // _putenv_s("OPENCV_FFMPEG_CAPTURE_OPTIONS", "");
-                _cameraCapture = new VideoCapture("rtsp://admin:Face1234@192.168.5.49:554/onvif1", VideoCapture.API.Ffmpeg);                
+                cameraCapture = new VideoCapture("rtsp://admin:Face1234@192.168.5.49:554/onvif1", VideoCapture.API.Ffmpeg);
                 //_cameraCapture = new VideoCapture("http://192.168.1.90:81/stream?x.mjpeg", VideoCapture.API.Any);
                 //_cameraCapture = new VideoCapture("rtsp://192.168.1.90:8554", VideoCapture.API.Ffmpeg);
                 //_cameraCapture = new VideoCapture("http://192.168.1.90/?x.mjpeg", VideoCapture.API.Ffmpeg);
@@ -57,7 +57,6 @@ namespace VideoSurveillance
                 // _cameraCapture.Read(_frame);
                 //// _cameraCapture.Retrieve(_frame, 0);
                 // _frame.CopyTo(_frameCopy);              
-
             }
             catch (Exception e)
             {
@@ -65,26 +64,26 @@ namespace VideoSurveillance
                 return;
             }
 
-            _fgDetector = new BackgroundSubtractorMOG2();
-            _blobDetector = new CvBlobDetector();
-            _tracker = new CvTracks();
+            fgDetector = new BackgroundSubtractorMOG2();
+            blobDetector = new CvBlobDetector();
+            tracker = new CvTracks();
 
             Application.Idle += ProcessFrame;
         }
 
         static bool frameProcessing = false;
-        static Mat LastProcessedFrame = new Mat();
-        static Thread Twsdetecting;
-        
-        public static void detectAndDisplayFunc(Mat frame)
-        {           
+        static Mat lastProcessedFrame = new Mat();
+        static Thread twsdetecting;
+
+        public static void DetectAndDisplayFunc(Mat frame)
+        {
             long detectionTime;
             List<Rectangle> faces = new List<Rectangle>();
             List<Rectangle> eyes = new List<Rectangle>();
             DetectFace.Detect(
-           frame, "haarcascade_frontalface_default.xml", "haarcascade_eye.xml",
-           faces, eyes,
-           out detectionTime);
+                frame, "haarcascade_frontalface_default.xml", "haarcascade_eye.xml",
+                faces, eyes,
+                out detectionTime);
 
             //paint faces
             foreach (Rectangle face in faces)
@@ -93,29 +92,29 @@ namespace VideoSurveillance
             //paint eyes
             foreach (Rectangle eye in eyes)
                 CvInvoke.Rectangle(frame, eye, new Bgr(Color.Blue).MCvScalar, 2);
-            LastProcessedFrame = frame;
+            lastProcessedFrame = frame;
             frameProcessing = false;
         }
 
         void ProcessFrame(object sender, EventArgs e)
         {
-            if (!_cameraCapture.IsOpened)
+            if (!cameraCapture.IsOpened)
                 return;
 
-            Mat frame = _cameraCapture.QueryFrame();
+            Mat frame = cameraCapture.QueryFrame();
 
             if (frame == null)
                 return;
             if (frameProcessing)
                 return;
 
-            imageBox1.Image = LastProcessedFrame;
+            imageBox1.Image = lastProcessedFrame;
 
             //skip frames due to poor performance       
             var twsDetectThread = new ThreadDetectFace(frame);           
             frameProcessing = true;
-            Twsdetecting = new Thread(new ThreadStart(twsDetectThread.CallDetect));
-            Twsdetecting.Start();
+            twsdetecting = new Thread(twsDetectThread.CallDetect);
+            twsdetecting.Start();
 
             //filter out noises
             //  Mat smoothedFrame = new Mat();
@@ -137,20 +136,20 @@ namespace VideoSurveillance
     public class ThreadDetectFace
     {
         // State information used in the task.
-        
-        Mat _frame;
+
+        private readonly Mat frame;
 
         // The constructor obtains the state information.
         public ThreadDetectFace(Mat frame)
         {
-            _frame = frame;            
+            this.frame = frame;            
         }
 
         // The thread procedure performs the task, such as formatting
         // and printing a document.
         public void CallDetect()
         {
-            FaceDetect.detectAndDisplayFunc(_frame);
+            FaceDetect.DetectAndDisplayFunc(frame);
         }
 
     }
