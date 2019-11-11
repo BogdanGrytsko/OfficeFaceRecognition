7bee797d93ceb70b7ecefb3f250fea393e716225
Mykola <Mykola.Gashevskyi@arrowdigital.com>
2019-11-11 13:50:05 +0200

    Fix RTSP UDP; Environment should be set before creating grabber

--- FaceRecognition/Video/VideoGrab.cs
+++ FaceRecognition/Video/VideoGrab.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using Emgu.CV;
 
 namespace FaceRecognition.Video
 {
     public class VideoGrab : IVideoGrab
-    {
-        [DllImport("msvcrt.dll")]
-        public static extern int _putenv_s(string e, string v);
-
+    {       
         private readonly VideoCapture videoCapture;
 
         public event Action<Mat> ImageGrabbed;
 
         public VideoGrab(string videoPath)
             : this(new VideoCapture(videoPath, VideoCapture.API.Ffmpeg))
-        {
+        {            
         }
 
         //captures webcam
@@ -25,8 +21,7 @@ namespace FaceRecognition.Video
         }
 
         private VideoGrab(VideoCapture videoCapture)
-        {
-            _putenv_s("OPENCV_FFMPEG_CAPTURE_OPTIONS", "rtsp_transport;udp");
+        {          
             this.videoCapture = videoCapture;
             videoCapture.ImageGrabbed += OnImageGrabbed;
         }
--- FaceRecognition/Video/VideoGrabFactory.cs
+++ FaceRecognition/Video/VideoGrabFactory.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.InteropServices;
 using FaceRecognition.Storage;
 
 namespace FaceRecognition.Video
 {
     public class VideoGrabFactory
     {
+        [DllImport("msvcrt.dll")]
+        public static extern int _putenv_s(string e, string v);
+
         private const string oldCamera = "rtsp://admin:Face1234@192.168.5.49:554/onvif1", newCamera = "rtsp://192.168.5.5:8554/mjpeg/1";
 
         public static IVideoGrab GetOldCamera()
         {
+            _putenv_s("OPENCV_FFMPEG_CAPTURE_OPTIONS", "rtsp_transport;udp"); // setup environment to decode RTSP UDP before videograbber creating 
             return new VideoGrab(oldCamera);
         }
 
